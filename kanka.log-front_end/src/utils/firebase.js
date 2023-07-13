95e53ef0870dc6d29e4de0221eb053dff4c57d50
NadavSofer <nadavsof5@gmail.com>
2023-07-13 14:45:32 +0300

    first commit

--- kanka.log-front_end/src/utils/firebase.js
+++ kanka.log-front_end/src/utils/firebase.js
@@ -11,13 +11,13 @@ import {getFirestore } from '@firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_API_KEY || 'AIzaSyAtz35RI5X4QD2XHMmBQrrJgbxqOAB5b28',
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN || 'kankalog-72ff6.firebaseapp.com',
+    projectId: process.env.REACT_APP_PROJECT_ID || 'kankalog-72ff6',
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET || 'kankalog-72ff6.appspot.com',
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || '227652470641',
+    appId: process.env.REACT_APP_APP_ID || '1:227652470641:web:0316409b065aa18d01d8d0',
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID || 'G-CGY7FBP1JS'
 };
 
 // Initialize Firebase
